2379f8526cb89bde4d627756b3e95413d64236cb
zixu <854066271@qq.com>
2020-07-15 21:15:59 +0800

    tabbar

---
+++ src/components/MainTabBar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- path是跳转的路径，activeColor是导航处于活跃时的颜色，不传默认teal色 -->
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="red">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/homefill.svg" alt />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/img/tabbar/rank.svg" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/rankfill.svg" alt />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/cart.svg" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/cartfill.svg" alt />
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/my.svg" alt />
+      <img slot="item-icon-active" src="~assets/img/tabbar/myfill.svg" alt />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from "components/tabbar/TabBar";
+import TabBarItem from "components/tabbar/TabBarItem";
+export default {
+  name: "MainTabBar",
+  components: {
+    TabBar,
+    TabBarItem
+  }
+};
+</script>
